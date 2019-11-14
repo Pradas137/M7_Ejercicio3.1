3ab0d4e18c242f3fa12f670dd467ef3ecc92ffaf
Adrian Pradas <pradas137@gmail.com>
2019-11-14 18:02:09 +0100

    first commit

--- datos.php
+++ datos.php
@@ -1,31 +1,35 @@
 <html>
  <head>
  	<title>Exemple de lectura de dades a MySQL</title>
+ 	<style>
+ 		.paises{
+
+ 			border: 1px solid black;
+ 		}
+ 	</style>
  </head>
  
  <body>
- 	<h1>Exemple de lectura de dades a MySQL</h1>
  	<?php
- 
-  	$mysqli = new mysqli('localhost', 'ap', 'Hakantor', 'world');
-  	?>
-  	<div align="left">
-  		<p>Paises:
-  			<select>
-  				<option value="0">Seleccione:</option>
-  				<?php
-  				$query = $mysqli -> query ("SELECT * FROM country");
-  					while ($valores = mysqli_fetch_array($query)) {
-  						echo '<option value="'.$valores[Code].'">'.$valores[Name].'</option>';
-  					}
-  				?>
-  			</select>
-  		<button>Enviar</button>
-  		<?php
-  				$query = $mysqli -> query ("SELECT * FROM country");
-  					while ($valores = mysqli_fetch_array($query)) {
-  						echo '<option value="'.$valores[Code].'">'.$valores[Name].'</option>';
-  					}
-  		?>
- </body>
+  		$mysqli = new mysqli('localhost', 'ap', 'Hakantor', 'world');
+	?>
+ 	<div align="left">                        
+ 	<form>
+      	<select name="ccode">
+        	<option value="0">Paises:</option>
+        	<?php
+          		$query = $mysqli -> query ("SELECT * FROM country");
+          		while ($valores = mysqli_fetch_array($query)) {
+            	echo '<option value="'.$valores[Code].'">'.$valores[Name].'</option>';}
+        	?>
+      	</select>
+      	<button>Enviar</button>
+    </form>
+        	<?php
+          		$city = $mysqli -> query ("SELECT Name, CountryCode FROM City");
+          		while ($valores = mysqli_fetch_array($city)) {
+            	echo '<option value="'.$valores[Code].'">'.$valores[Name].'</option>';}
+        	?>
+  </div>
+</body>
 </html>
